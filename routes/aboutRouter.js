7dc0d0e82312490cdbb9f3e50c1d255beeb8b8d2
Casey Burden <caseyburden@me.com>
2014-07-06 22:45:48 -0400

    Almost done with About

--- routes/aboutRouter.js
+++ routes/aboutRouter.js
@@ -1,13 +1,33 @@
 var router = require('express').Router();
 
+var facultyData = require('../data/faculty.json');
 
-router.get('/', function(req, res){
-	res.render('aboutus');
+
+
+router.get('/faculty', function(req, res){
+	res.render('about/faculty',{faculty: facultyData});
+})
+
+router.get('/degree-granting', function(req, res){
+	res.render('about/degree-granting');
+})
+
+router.get('/accreditation', function(req, res){
+	res.render('about/accreditation');
 })
 
 router.get('/advantage', function(req, res){
 	res.render('about/advantages');
 })
+
+router.get('/mission', function(req, res){
+	res.render('about/mission');
+})
+
+router.get('/', function(req, res){	
+	res.redirect('about/advantage');
+})
+
 
 
 module.exports = router;
---
+++ routes/errorPageRouter.js
@@ -0,0 +1,51 @@
+var router = require('express').Router();
+
+router.get('/404', function(req, res, next){
+  // trigger a 404 since no other middleware
+  // will match /404 after this one, and we're not
+  // responding here
+  next();
+});
+
+router.get('/403', function(req, res, next){
+  // trigger a 403 error
+  var err = new Error('not allowed!');
+  err.status = 403;
+  next(err);
+});
+
+router.get('/500', function(req, res, next){
+  // trigger a generic (500) error
+  next(new Error('keyboard cat!'));
+});
+
+
+
+router.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+router.use(function(err, req, res, next){
+  // we may use properties of the error object
+  // here and next(err) appropriately, or if
+  // we possibly recovered from the error, simply next().
+  res.status(err.status || 500);
+  res.render('500', { error: err });
+});
+
+module.exports = router;
